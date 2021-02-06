55b87ac5bb5a029a7a7f08aea7e0889402431365
ayserjamshidi <acejamshidi4th@gmail.com>
2021-02-06 17:51:59 -0500

    -Created Lab 2 directory and assignment. -Added comments to every function. -Adjusted Lab 1 test cases to have more clarity if what is being output. -Recreated reverse(int, int) to be more efficient. -Recreated evens_on_left(int[], int) to be more efficient.

--- Assignment 1 Warmup/ArrayPractice.c
+++ Assignment 1 Warmup/ArrayPractice.c
@@ -33,8 +33,6 @@ int main(int argc, char **argv) {
 	int A[] = {9, 505, 321, 117, 27, 64, 13, 9, 12, 208};
 	int B[LEN_A + 1];
 	int newValue = 505;
-	printf("\nSecond largest is %i\n", secondLargest(A, LEN_A));
-
 	printf("A[] is: ");
 	print_array(A, LEN_A); // Expect: (9, 505, 321, 117, 27, 64, 13, 9, 12, 208)
 
@@ -43,7 +41,7 @@ int main(int argc, char **argv) {
 	print_array(B, LEN_A + 1); // Expect: (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 	array_copy(B, A, LEN_A);
-	printf("\nbut now it's ");
+	printf("\nbut copying all of A to B, now B is ");
 	print_array(B, LEN_A + 1); // Expect (9, 505, 321, 117, 27, 64, 13, 9, 12, 208, 0)
 
 	insert(B, newValue, 0, LEN_A);
@@ -51,17 +49,17 @@ int main(int argc, char **argv) {
 	print_array(B, LEN_A + 1); // Expect (505, 9, 505, 321, 117, 27, 64, 13, 9, 12, 208)
 
 	printf("\nThe average of A[] is %f", average(A, LEN_A)); // Expect 128.5d
-	printf("\nThere are %d items in A[] less than this.", less_than_average(A, LEN_A)); // expect 7
+	printf(" and there are %d items in A[] less than this.", less_than_average(A, LEN_A)); // expect 7
 
-	printf("\n");
+	printf("\nA[] reversed is ");
 	reverse(A, LEN_A);
 	print_array(A, LEN_A); // Expect (208, 12, 9, 13, 64, 27, 117, 321, 505, 9)
 
-	printf("\n");
+	printf("\nA[] rotated 3 to the right is: ");
 	rotate_right(A, LEN_A, 3);
 	print_array(A, LEN_A); // Expect (321, 505, 9, 208, 12, 9, 13, 64, 27, 117)
 
-	printf("\n");
+	printf("\nA[] evens on the left is: ");
 	evens_on_left(A, LEN_A); // Expect (1358888978, 64, 12, 208, 117, 27, 13, 9, 9, 505)
 	print_array(A, LEN_A);
 
@@ -71,20 +69,20 @@ int main(int argc, char **argv) {
 	printf("\n");
 	print_array(A, LEN_A);
 	if (contains_dup(A, LEN_A))
-		printf("\nA[] contains a dup");
+		printf(" - A[] contains a duplicate");
 	else
-		printf("\nA[] doesn't contain a dup");
+		printf(" - A[] doesn't contain a duplicate");
 
 	printf("\n");
 	print_array(B, sizeof(B)/sizeof(*B));
 	if (contains_dup(B, LEN_A + 1))
-		printf("\nB[] contains a dup");
+		printf(" - B[] contains a duplicate");
 	else
-		printf("\nB[] doesn't contain a dup");
+		printf("- B[] doesn't contain a duplicate");
 
 	printf("\n");
 	print_array(A, sizeof(A)/sizeof(*A));
-	printf("\nSecond largest is %i\n", secondLargest(A, LEN_A));
+	printf("\nA[]'s second largest is %i\n", secondLargest(A, sizeof(A)/sizeof(*A))); // Expect 321
 	return 0;
 }
 
@@ -95,8 +93,8 @@ int main(int argc, char **argv) {
 void print_array(int A[], int length) {
 	printf("(");
 
-	for (int i = 0; i < length; i++)
-		printf((i < length - 1) ? "%i, " : "%i", A[i]);
+	for (int i = 0; i < length; i++) // Loop through every element in A.
+		printf((i < length - 1) ? "%i, " : "%i", A[i]); // If we're at the last element, don't add a comma and space.
 
 	printf(")"); // We keep this ending parenthesis at the end just in case the array is empty
 }
@@ -105,20 +103,20 @@ void print_array(int A[], int length) {
 double average(int A[], int length) {
 	double arraySum = 0;
 
-	for (int i = 0; i < length; i++)
-		arraySum += A[i];
+	for (int i = 0; i < length; i++) // Loop through every element in A
+		arraySum += A[i]; // Add the current element to our total
 
-	return arraySum / length;
+	return arraySum / length; // Divide our total with the amount of elements in the array
 }
 
 /* returns the number of items in A that are less than the
    average of the items in A */
 int less_than_average(int A[], int length) {
-	double arrayAvg = average(A, length);
+	double arrayAvg = average(A, length); // Get average
 	int lowerThanAvg = 0;
 
-	for (int i = 0; i < length; i++)
-		if (A[i] < arrayAvg)
+	for (int i = 0; i < length; i++) // Loop through every elemnt in A
+		if (A[i] < arrayAvg) // If the current element is less than the stores average
 			lowerThanAvg++;
 
 	return lowerThanAvg;
@@ -128,20 +126,13 @@ int less_than_average(int A[], int length) {
    A[] contained [1,2,3,4,5], after the function, A[] contains
    [5,4,3,2,1] */
 void reverse(int A[], int length) {
-	//int reversedArrContainer[length];
+	for (int i = 0; i < length / 2; i++) { // Loop through every number before the half-way point of A's length.
+		int container = A[i]; // Store the current element as it will be overwritten.
 
-	for (int i = 0; i < length; i++) {
-		int container = A[i];
-
-		A[i] = A[length - i];
-		A[length - i] = container;
+		// Swap the two elements with each other
+		A[i] = A[length - 1 - i];
+		A[length - 1 - i] = container;
 	}
-
-	/*for (int i = 0; i < length; i++)
-		reversedArrContainer[length - 1 - i] = A[i];
-
-	for (int i = 0; i < length; i++)
-		A[i] = reversedArrContainer[i];*/
 }
 
 /* returns the second largest item in A[], or -1 if len < 2 */
@@ -149,32 +140,29 @@ int secondLargest(int A[], int length) {
 	if (length < 2)
 		return -1;
 
-	int container[2] = { A[0], -2147483648 }; // First number in the array with the smallest possible int
+	int container[2] = {A[0], -2147483648}; // First number in the array, then the smallest possible int
 
-	for (int i = 1; i < length; i++) {
-		printf("\n%i > %i == %i\n", A[i], container[0], (A[i] > container[0]));
-		if (A[i] > container[0]) {
+	for (int i = 1; i < length; i++) { // Loop every element after the first element
+		if (A[i] > container[0]) { // Check if the current element is bigger than the previous largest element
 			// Shift container numbers
-			container[1] = container[0];
-			container[0] = A[i];
-		} else if (A[i] > container[1])
-			container[1] = A[i];
+			container[1] = container[0]; // Move current largest to second largest
+			container[0] = A[i]; // Assign current largest number to current element
+		} else if (A[i] > container[1]) // Check if the current element is larger than the second largest number
+			container[1] = A[i]; // Assign second largest number to current element
 	}
 
-	/* for now return 0 as a placeholder.  We do this just so that the program
-	   compiles without warning until we've finished */
-	return container[1];
+	return container[1]; // Return the second largest number
 }
 
 /* rotates the values in the array numPlaces to the right */
 /* for example, if A[] is initially 10,20,30,40,50, and numPlaces=2 */
 /* after the function finishes, A[] would be 40,50,10,20,30 */
 void rotate_right(int A[], int length, int numPlaces) {
-	if (length <= 1)
+	if (length <= 1) // Ensure there's anything to rotate.
 		return;
 
-	if (numPlaces >= length) // Avoid weird stuff happening when numPlaces is greater than the length
-		numPlaces = numPlaces % length;
+	if (numPlaces >= length) // Avoid weird stuff happening when numPlaces is greater than the length.
+		numPlaces = numPlaces % length; // Set numPlaces to the remainder as it will have the same desired effect.
 
 	int rotatedContainer[numPlaces];
 	int containerIndex = 0;
@@ -223,22 +211,16 @@ void initialize(int A[], int len, int initialValue) {
 /* shuffles the values in A so that the even values
    are all to the left of the odd values */
 void evens_on_left(int A[], int len) {
-	int evenContainer[len], oddContainer[len];
-	int evenIndex = 0, oddIndex = 0;
+	for (int i = 0; i < len; i++) {
+		int temp = A[i]; // Stores current element as we will possibly be overriding it.
 
-	for (int i = len; i > 0; i--)
-		if (A[i] % 2 == 0) // Number is even
-			evenContainer[evenIndex++] = A[i];
-		else // Odd
-			oddContainer[oddIndex++] = A[i];
+		if (temp % 2 == 0) { // If current number is even.
+			for (int j = i; j > 0; j--) // Shift every element behind this element to the right once.
+				A[j] = A[j - 1]; // Assign the current element to the previous element.
 
-	oddIndex = 0;
-
-	for (int i = 0; i < len; i++)
-		if (i < evenIndex) // Even insertion
-			A[i] = evenContainer[i];
-		else // Odd insertion
-			A[i] = oddContainer[oddIndex++];
+			A[0] = temp; // Assign the first element to be the current even number.
+		}
+	}
 }
 
 /* returns the index of the first occurrence of
@@ -264,7 +246,7 @@ int find_last(int A[], int len, int x) {
 /* returns 1 if there is at least one element in A[]
    that is contained elsewhere in A[], or 0 if there isn't */
 int contains_dup(int A[], int len) {
-	for (int i = 0; i < len; i++)
+	for (int i = 0; i < len; i++) // Basic linear search
 		for (int j = 0; j < len; j++)
 			if (A[i] == A[j])
 				return 1;
